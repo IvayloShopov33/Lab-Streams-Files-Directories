44017896e09d7942242c467857292f276244c443
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-09-21 23:13:56 +0300

    Add files via upload

---
+++ 4. MergeFiles/MergeFiles.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace MergeFiles
+{
+    public class MergeFiles
+    {
+        static void Main()
+        {
+            var firstInputFilePath = @"..\..\..\Files\input1.txt";
+            var secondInputFilePath = @"..\..\..\Files\input2.txt";
+            var outputFilePath = @"..\..\..\Files\output.txt";
+
+            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
+        }
+
+        public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
+        {
+            using StreamReader firstInput = new StreamReader(firstInputFilePath);
+            using StreamReader secondInput = new StreamReader(secondInputFilePath);
+            using StreamWriter output = new StreamWriter(outputFilePath);
+
+            //merge two files' values, line by line into a third text file
+            while (!firstInput.EndOfStream || !secondInput.EndOfStream)
+            {
+                if (!firstInput.EndOfStream)
+                {
+                    string firstLine = firstInput.ReadLine();
+                    output.WriteLine(firstLine);
+                    Console.WriteLine(firstLine);
+                }
+
+                if (!secondInput.EndOfStream)
+                {
+                    string secondLine = secondInput.ReadLine();
+                    output.WriteLine(secondLine);
+                    Console.WriteLine(secondLine);
+                }
+            }
+        }
+    }
+}
