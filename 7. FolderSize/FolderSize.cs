aa2fc47a67de759096bd27239365a808718e469f
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-09-23 01:27:33 +0300

    Add files via upload

---
+++ 7. FolderSize/FolderSize.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+
+namespace FolderSize
+{
+    public class FolderSize
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"..\..\..\Files\TestFolder";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            GetFolderSize(folderPath, outputPath);
+        }
+
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+            //convert the total sum from bytes to kilobytes
+            decimal sumOfFileSizes = CalculateTotalSumOfFileSizes(folderPath) / 1024.0m;
+            string outputText = $"{sumOfFileSizes} KB";
+            File.WriteAllText(outputFilePath, outputText);
+
+        }
+
+        static long CalculateTotalSumOfFileSizes(string folderPath)
+        {
+            string[] files = Directory.GetFiles(folderPath);
+            long totalSumOfFileSizes = 0;
+            //add the sizes of files and directories to the total sum
+            foreach (string file in files)
+            {
+                totalSumOfFileSizes += new FileInfo(file).Length;
+            }
+            
+            string[] subDirectories = Directory.GetDirectories(folderPath);
+            foreach (string subDirectory in subDirectories)
+            {
+                totalSumOfFileSizes += CalculateTotalSumOfFileSizes(subDirectory);
+            }
+
+            return totalSumOfFileSizes;
+        }
+    }
+}
