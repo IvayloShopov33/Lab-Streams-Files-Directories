24ea5d0bc019a0b3236b2d3b48611d11e6240249
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-09-22 23:25:09 +0300

    Add files via upload

---
+++ 5. ExtractSpecialBytes/ExtractSpecialBytes.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace ExtractSpecialBytes
+{
+    public class ExtractSpecialBytes
+    {
+        static void Main()
+        {
+            string binaryFilePath = @"..\..\..\Files\example.png";
+            string bytesFilePath = @"..\..\..\Files\bytes.txt";
+            string outputPath = @"..\..\..\Files\output.bin";
+
+            ExtractBytesFromBinaryFile(binaryFilePath, bytesFilePath, outputPath);
+        }
+
+        public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
+        {
+            using FileStream fileReader = new FileStream(bytesFilePath, FileMode.Open, FileAccess.Read, FileShare.None);
+            using FileStream fileWriter = new FileStream(outputPath, FileMode.Create, FileAccess.Write, FileShare.None);
+
+            byte[] bytes = new byte[fileReader.Length];
+            fileReader.Read(bytes);
+            fileWriter.Write(bytes);
+        }
+    }
+}
